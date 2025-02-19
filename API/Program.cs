0217803309315b921287fa9add8af1c06a63fb10
Nicholas Carvalho <nicholasjacarvalho@gmail.com>
2025-02-19 05:08:14 -0500

    Section 3 Complete

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,11 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
-
 app.Run();
